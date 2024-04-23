2bd6b97aa05fae2ec22de3183f176a2156cef29a
fidelalejandro <alejandro.garcia.garcia0@rai.usc.es>
2024-04-23 17:32:26 +0200

    eliminar y modificar grupo en el DAO

--- elDuroGym/src/baseDatos/DAOGrupo.java
+++ elDuroGym/src/baseDatos/DAOGrupo.java
@@ -45,4 +45,67 @@ public class DAOGrupo extends AbstractDAO {
             }
         }
     }
+
+    public void eliminarGrupo(int idGrupo) {
+        Connection con = null;
+        PreparedStatement stmEliminar = null;
+
+        try {
+            con = this.getConexion();
+            stmEliminar = con.prepareStatement("DELETE FROM Grupo WHERE id_grupo = ?");
+            stmEliminar.setInt(1, idGrupo);
+            stmEliminar.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+        } finally {
+            // Cerrar recursos
+            try {
+                if (stmEliminar != null) {
+                    stmEliminar.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Imposible cerrar cursores");
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("Imposible cerrar conexión");
+                }
+            }
+        }
+    }
+
+    public void modificarGrupo(Grupo grupo) {
+        Connection con = null;
+        PreparedStatement stmModificar = null;
+
+        try {
+            con = this.getConexion();
+            stmModificar = con.prepareStatement("UPDATE Grupo SET id_actividad = ? WHERE id_grupo = ?");
+            stmModificar.setInt(1, grupo.getActividad().getId_Actividad());
+            stmModificar.setInt(2, grupo.getId_Grupo());
+            stmModificar.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+        } finally {
+            // Cerrar recursos
+            try {
+                if (stmModificar != null) {
+                    stmModificar.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Imposible cerrar cursores");
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("Imposible cerrar conexión");
+                }
+            }
+        }
+    }
 }
